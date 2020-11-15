845fc741c3bef2361593f680e1d4869a3ca58f56
Raychell Valeria Arguedas Bolivar <valearguedas03@gmail.com>
2020-11-15 14:42:38 -0600

    avance 1

--- rutas.py
+++ rutas.py
@@ -1,2 +1,12 @@
 def encontrar_ruta(C):
-    raise NotImplementedError()
+    matriz = testMatriz(C, 0, 0)
+    if matriz == []:
+        return []
+    else: 
+        for i in range(0, len(matriz)):
+            for j in range (0, len(matriz[0])):
+                if matriz[i][j] == 'b':
+                    matriz[i][j] = 1
+                else:
+                    matriz[i][j] = 0
+    return matriz
